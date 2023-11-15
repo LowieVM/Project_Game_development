5912a9e4ba308b18689fa74aaa8c38ba70cbdaf4
Lowie Van Meensel <lowie366@gmail.com>
2023-11-15 11:55:35 +0100

    add MouseReader

---
+++ Project_Game_development/MouseReader.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal class MouseReader
+    {
+        public Vector2 GetMouseVector()
+        {
+            MouseState state = Mouse.GetState();
+            return new Vector2(state.X, state.Y);
+        }
+    }
+}
