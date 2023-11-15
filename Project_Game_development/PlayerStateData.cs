536ea6179a365261ef93d3c009c54840ff5219a4
Lowie Van Meensel <lowie366@gmail.com>
2023-11-15 17:00:25 +0100

    add PlayerStateData class

---
+++ Project_Game_development/PlayerStateData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal class PlayerStateData
+    {
+        public Texture2D Texture { get; set; }
+        public Animation Animation { get; set; }
+        public Vector2 RotationPoint { get; set; }
+
+        public PlayerStateData(Texture2D texture, Animation animation, Vector2 rotationPoint)
+        {
+            Texture = texture;
+            Animation = animation;
+            RotationPoint = rotationPoint;
+        }
+    }
+}
