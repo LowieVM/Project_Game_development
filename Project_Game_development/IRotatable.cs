d57bbbfe33fc3d8ada8ce0a41a9b186ec22c5084
Lowie Van Meensel <lowie366@gmail.com>
2023-11-15 14:05:55 +0100

    add IRotatable

---
+++ Project_Game_development/IRotatable.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Project_Game_development
+{
+    internal interface IRotatable
+    {
+        public float Rotation { get; set; }
+        public Vector2 RotationPoint { get; set; }
+    }
+}
