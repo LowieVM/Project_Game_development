2b57d22436eb695d2b8b52310a6e861bf5195b13
Lowie Van Meensel <lowie366@gmail.com>
2023-11-15 05:57:46 +0100

    add AnimationFrame

---
+++ Project_Game_development/Content/AnimationFrame.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Project_Game_development.Content
+{
+    internal class AnimationFrame
+    {
+        public Rectangle SourceRectangle { get; set; }
+
+        public AnimationFrame(Rectangle rectangle)
+        {
+            SourceRectangle = rectangle;
+        }
+    }
+}
