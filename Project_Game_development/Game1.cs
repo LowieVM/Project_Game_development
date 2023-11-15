91379d19bb2d2d1fb51d1557f058fbb0c496ea29
Lowie Van Meensel <lowie366@gmail.com>
2023-11-15 10:43:22 +0100

    add player textures in Game1

--- Project_Game_development/Game1.cs
+++ Project_Game_development/Game1.cs
@@ -8,9 +8,18 @@ namespace Project_Game_development
     {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
+
         private Texture2D playerWalkTexture;
         private Texture2D playerRunTexture;
+        private Texture2D playerPistolTexture;
+        private Texture2D playerShotgunTexture;
+        private Texture2D playerShotgunReloadTexture;
+        private Texture2D playerMinigunTexture;
+        private Texture2D playerMinigunShootTexture;
+        private Texture2D playerFlamethrowerTexture;
+
         private Player player;
+
 
         public Game1()
         {
@@ -23,7 +32,7 @@ namespace Project_Game_development
         {
             // TODO: Add your initialization logic here
             base.Initialize();
-            player = new Player(playerWalkTexture, playerRunTexture);
+            player = new Player(playerWalkTexture, playerRunTexture, playerPistolTexture, playerShotgunTexture, playerShotgunReloadTexture, playerMinigunTexture, playerMinigunShootTexture, playerFlamethrowerTexture);
         }
 
         protected override void LoadContent()
@@ -33,6 +42,12 @@ namespace Project_Game_development
             // TODO: use this.Content to load your game content here
             playerWalkTexture = Content.Load<Texture2D>("player_walk_strip6");
             playerRunTexture = Content.Load<Texture2D>("player_run_strip6");
+            playerPistolTexture = Content.Load<Texture2D>("player_9mmhandgun");
+            playerShotgunTexture = Content.Load<Texture2D>("player_pumpgun_stand");
+            playerShotgunReloadTexture = Content.Load<Texture2D>("player_pumgun_reload_strip5");
+            playerMinigunTexture = Content.Load<Texture2D>("player_chaingun");
+            playerMinigunShootTexture = Content.Load<Texture2D>("player_chaingun_shoot_strip2");
+            playerFlamethrowerTexture = Content.Load<Texture2D>("player_flamethrower");
         }
 
         protected override void Update(GameTime gameTime)
