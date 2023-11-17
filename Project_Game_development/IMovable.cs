af019a991edf1395f59e7457890010d13f94e275
Lowie Van Meensel <lowie366@gmail.com>
2023-11-17 02:34:13 +0100

    add IMowable

---
+++ Project_Game_development/IMovable.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal interface IMovable
+    {
+        Vector2 Position { get; set; }
+        Vector2 InitialSpeed { get; set; }
+        float MaxSpeed { get; set; }
+        Vector2 MoveDirection { get; set; }
+        Vector2 Acceleration { get; set; }
+    }
+}
