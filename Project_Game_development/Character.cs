9b3c9a5e03b48abd8eb54bf2dbb4ee16c9166ba9
Lowie Van Meensel <lowie366@gmail.com>
2023-11-18 15:16:52 +0100

    implement interfaces in Character

--- Project_Game_development/Character.cs
+++ Project_Game_development/Character.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,19 @@ using System.Threading.Tasks;
 
 namespace Project_Game_development
 {
-    internal abstract class Character
+    internal abstract class Character : IRotatable, IMovable
     {
+        public float Rotation { get; set; }
+        public Vector2 RotationPoint { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 InitialSpeed { get; set; }
+        public float MaxSpeed { get; set; }
+        public Vector2 MoveDirection { get; set; }
+        public Vector2 Acceleration { get; set; }
 
+        public Character()
+        {
+
+        }
     }
 }
