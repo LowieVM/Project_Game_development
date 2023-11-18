25abd33e947452abaed8cecd80d55702f87a84d0
Lowie Van Meensel <lowie366@gmail.com>
2023-11-18 11:09:24 +0100

    add GameSetings static class

---
+++ Project_Game_development/GameSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal static class GameSettings
+    {
+        public static int ScreenWidth { get; set; } = 1920;
+        public static int ScreenHeight { get; set; } = 1080;
+    }
+}
