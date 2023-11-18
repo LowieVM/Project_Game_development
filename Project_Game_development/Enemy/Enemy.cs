d63552cc2bfc31a548f20b7edb295f6224462da7
Lowie Van Meensel <lowie366@gmail.com>
2023-11-18 14:26:15 +0100

    add draw and update to Enemy

--- Project_Game_development/Enemy/Enemy.cs
+++ Project_Game_development/Enemy/Enemy.cs
@@ -11,15 +11,19 @@ namespace Project_Game_development
     enum EnemyState { Walking, WalkingPistol, Pistol, Dying }
     internal class Enemy : IRotatable, IMovable
     {
-        public Vector2 Position { get; set; }
-        public Vector2 InitialSpeed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public float MaxSpeed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Vector2 MoveDirection { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Vector2 Acceleration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public float Rotation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Vector2 RotationPoint { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Vector2 Position { get; set; } = new Vector2(200, 200);
+        public Vector2 InitialSpeed { get; set; } = new Vector2(5, 5);
+        public float MaxSpeed { get; set; } = 10;
+        public Vector2 MoveDirection { get; set; } = new Vector2(0, 0);
+        public Vector2 Acceleration { get; set; } = new Vector2(0.1f, 0.1f);
+        public float Rotation { get; set; } = 0;
+        public Vector2 RotationPoint { get; set; } = new Vector2(0, 0);
 
         private Dictionary<EnemyState, SpriteProperties> EnemyStateMappings;
+        private SpriteEffects effect = SpriteEffects.None;
+        private Texture2D currentTexture;
+        private Animation currentAnimation;
+        private EnemyState currentState;
         public Enemy(Texture2D walkTexture, Texture2D walkPistol, Texture2D pistolTexture, Texture2D dieTexture)
         {
             EnemyStateMappings = new Dictionary<EnemyState, SpriteProperties>
@@ -29,16 +33,22 @@ namespace Project_Game_development
                 { EnemyState.Pistol, new SpriteProperties(pistolTexture, new Vector2(26, 31)) },
                 { EnemyState.Dying, new SpriteProperties(dieTexture, 12, 4, 1, new Vector2(34, 26)) },
             };
+            currentState = EnemyState.Pistol;
+            currentTexture = EnemyStateMappings[currentState].Texture;
+            currentAnimation = EnemyStateMappings[currentState].Animation;
         }
 
-        public void Update()
+        public void Update(GameTime gameTime)
         {
-
+            currentTexture = EnemyStateMappings[currentState].Texture;
+            currentAnimation = EnemyStateMappings[currentState].Animation;
+            RotationPoint = EnemyStateMappings[currentState].RotationPoint;
+            currentAnimation.Update(gameTime);
         }
 
-        public void Draw()
+        public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Draw(currentTexture, Position, currentAnimation.CurrentFrame.SourceRectangle, Color.White, Rotation, RotationPoint, 1f, effect, 0f);
         }
     }
 }
