ebdbca4cb102d54e1530795dfe8a6cb4decc61da
Lowie Van Meensel <lowie366@gmail.com>
2023-11-19 13:14:39 +0100

    add MoveBehavior class

---
+++ Project_Game_development/MoveBehavior.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal class MoveBehavior
+    {
+        public Vector2 Target { get; set; }
+        public void UpdateDirection(IPositional from, IPositional to)
+        {
+
+        }
+    }
+}
