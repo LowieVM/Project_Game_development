a7479f1cbfa956b466f7a2a2c6d57670c5230dc8
Lowie Van Meensel <lowie366@gmail.com>
2023-11-19 21:34:24 +0100

    add move behavior KeepDistance

---
+++ Project_Game_development/MoveBehavior/MoveBehaviorKeepDistance.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal class MoveBehaviorKeepDistance : MoveBehavior
+    {
+        public IPositional target { get; set; }
+        public override Vector2 MoveDirection { get; set; }
+
+        public MoveBehaviorKeepDistance(IPositional target)
+        {
+            this.target = target;
+        }
+
+        public override void UpdateMoveDirection(IMovable movable)
+        {
+            Vector2 direction = Vector2.Zero;
+
+            if (Vector2.Distance(movable.Position, target.Position) < 200)
+            {
+                direction = Vector2.Normalize(movable.Position - target.Position);
+            }
+            MoveDirection = direction;
+        }
+    }
+}
