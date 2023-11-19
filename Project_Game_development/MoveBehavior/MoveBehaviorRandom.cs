f5e5409ce0c9e205b2949b8e00fac212b166bcd9
Lowie Van Meensel <lowie366@gmail.com>
2023-11-19 22:05:17 +0100

    add random move behavior

---
+++ Project_Game_development/MoveBehavior/MoveBehaviorRandom.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal class MoveBehaviorRandom : MoveBehavior
+    {
+        private Vector2 initialDirection;
+        private Random random;
+
+        public override Vector2 MoveDirection { get; set; }
+
+        public MoveBehaviorRandom(IPositional initialDirection)
+        {
+            this.initialDirection = initialDirection.Position;
+            this.MoveDirection = initialDirection.Position;
+            this.random = new Random();
+        }
+
+        public override void UpdateMoveDirection(IMovable movable)
+        {
+            if (random.NextDouble() < 0.02)
+            {
+                MoveDirection = new Vector2((float)random.NextDouble() - 0.5f, (float)random.NextDouble() - 0.5f);
+                MoveDirection.Normalize();
+            }
+            else if (random.NextDouble() < 0.005)
+            {
+                MoveDirection = Vector2.Zero;
+            }
+        }
+    }
+}
