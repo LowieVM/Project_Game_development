2563ab560e49ab8735718cd5d66dbf027e6d3cfd
Lowie Van Meensel <lowie366@gmail.com>
2023-11-20 20:26:26 +0100

    add TestLevel

---
+++ Project_Game_development/TestLevel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal class TestLevel
+    {
+        private Player player;
+        private List<Officer> officers;
+        public TestLevel()
+        {
+            player = new Player(new Vector2(50,50));
+            officers.Add(new Officer(new Vector2(500, 500), player));
+        }
+        public void Update(GameTime gameTime)
+        {
+            player.Update(gameTime);
+            foreach (var officer in officers)
+            {
+                officer.Update(gameTime);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            player.Draw(spriteBatch);
+            foreach (var officer in officers)
+            {
+                officer.Draw(spriteBatch);
+            }
+        }
+    }
+}
