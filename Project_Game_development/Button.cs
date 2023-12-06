46a9f6ff13f82c6d92e21c291c0061febdde62fb
Lowie Van Meensel <lowie366@gmail.com>
2023-12-06 21:37:47 +0100

    add GameManager and Button class

---
+++ Project_Game_development/Button.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal class Button
+    {
+        private SpriteFont font;
+        private Vector2 startButtonPosition;
+        private Rectangle startButtonRectangle;
+        private string text;
+        private MouseState previousMouseState;
+
+        public Button(string text, Vector2 position)
+        {
+            this.text = text;
+            previousMouseState = Mouse.GetState();
+            font = GameTextures.ArialFont;
+
+            int width = (int)font.MeasureString(text).X;
+            int height = (int)font.MeasureString(text).Y;
+            startButtonPosition = new Vector2((position.X/2) - (width/2), (position.Y / 2) - (height / 2));
+            startButtonRectangle = new Rectangle(
+                (int)startButtonPosition.X,
+                (int)startButtonPosition.Y,
+                width,
+                height
+            );
+        }
+
+        public bool IsButtonClicked()
+        {
+            bool toReturn;
+            MouseState currentMouseState = Mouse.GetState();
+            toReturn = startButtonRectangle.Contains(currentMouseState.Position) && currentMouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released;
+            previousMouseState = currentMouseState;
+            return toReturn;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(font, text, startButtonPosition, Color.White, 0f, Vector2.Zero, 1, SpriteEffects.None, 0);
+        }
+    }
+}
---
+++ Project_Game_development/Managers/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    enum GameState{ Playing, GameOver, MainMenu}
+    internal class GameManager
+    {
+        TestLevel currentLevel;
+        Button start;
+        Button gameOver;
+        public GameState gameState { get; set; }
+        public GameManager()
+        {
+            gameState = GameState.MainMenu;
+            start = new Button("Start game", new Vector2(GameSettings.ScreenWidth, 200));
+            gameOver = new Button("Game over\n\nMain menu", new Vector2(GameSettings.ScreenWidth, GameSettings.ScreenHeight));
+        }
+        public void Update(GameTime gameTime)
+        {
+            if (start.IsButtonClicked() && gameState == GameState.MainMenu)
+            {
+                currentLevel = new TestLevel();
+                gameState = GameState.Playing;
+            }
+            if (gameOver.IsButtonClicked() && gameState == GameState.GameOver)
+            {
+                gameState = GameState.MainMenu;
+            }
+            if (gameState == GameState.Playing)
+            {
+                currentLevel.Update(gameTime);
+                if (!currentLevel.cManager.playerTeam[0].isAlive)
+                {
+                    gameState = GameState.GameOver;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (gameState == GameState.Playing)
+            {
+                currentLevel.Draw(spriteBatch);
+            }
+            if (gameState == GameState.MainMenu)
+            {
+                start.Draw(spriteBatch);
+            }
+            if (gameState == GameState.GameOver)
+            {
+                gameOver.Draw(spriteBatch);
+            }
+        }
+    }
+}
