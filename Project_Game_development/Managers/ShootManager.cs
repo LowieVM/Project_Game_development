a075f49e567dcbaec160f7b449fcbcb936d617c8
Lowie Van Meensel <lowie366@gmail.com>
2023-12-06 16:12:31 +0100

    add ShootManager abstract class

---
+++ Project_Game_development/Managers/ShootManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Game_development
+{
+    internal abstract class ShootManager
+    {
+        public BulletManager bulletManager { get; set; }
+        protected IPositional shooter;
+        public IPositional target { get; set; }
+
+        public ShootManager(IPositional shooter, List<IHittable> enemies)
+        {
+            bulletManager = new BulletManager(enemies);
+            target = enemies.FirstOrDefault();
+            this.shooter = shooter;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            bulletManager.Update(gameTime);
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            bulletManager.Draw(spriteBatch);
+        }
+    }
+}
