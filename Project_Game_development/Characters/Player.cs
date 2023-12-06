e4db15cd935cfd40fa1367a0e34b9b476c4860a9
Lowie Van Meensel <lowie366@gmail.com>
2023-12-06 12:05:39 +0100

    implement Die()

--- Project_Game_development/Characters/Player.cs
+++ Project_Game_development/Characters/Player.cs
@@ -22,20 +22,40 @@ namespace Project_Game_development
         }
 
 
+        protected override void Die()
+        {
+            isAlive = false;
+        }
+
+        private void DrawGameOverScreen(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(GameTextures.ArialFont, "Game Over", new Vector2(200, 200), Color.White, 0f, Vector2.Zero, 1, SpriteEffects.None, 0);
+        }
+
+
         public override void Update(GameTime gameTime)
         {
-            base.Update(gameTime);
+            if (isAlive)
+            {
+                base.Update(gameTime);
 
-            Keyboard.UpdateState(this);
-            mouseReader.Update();
-            playerShootManager.Update(gameTime);
+                Keyboard.UpdateState(this);
+                mouseReader.Update();
+                playerShootManager.Update(gameTime);
+            }
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            playerShootManager.Draw(spriteBatch);
-
-            base.Draw(spriteBatch);
+            if (isAlive)
+            {
+                playerShootManager.Draw(spriteBatch);
+                base.Draw(spriteBatch);
+            }
+            else
+            {
+                DrawGameOverScreen(spriteBatch);
+            }
         }
     }
 }
