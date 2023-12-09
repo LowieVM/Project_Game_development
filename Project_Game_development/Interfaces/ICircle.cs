b4a08c633b78b96abddecf05a2c0ee77173d5540
Lowie Van Meensel <lowie366@gmail.com>
2023-12-09 16:25:49 +0100

    add circle and rectangle interface

---
+++ Project_Game_development/Interfaces/ICircle.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Project_Game_development
+{
+    internal interface ICircle: IRotatable
+    {
+        public float CircleRadius { get; set; }
+    }
+}
---
+++ Project_Game_development/Interfaces/IRectangle.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Project_Game_development
+{
+    internal interface IRectangle
+    {
+        public Rectangle Rectangle { get; set; }
+    }
+}
